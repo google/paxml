b5a7be434e714acae55220de1edb1f45e9fceac5
pax authors <no-reply@google.com>
2022-06-21 15:17:39 -0700

    internal change
    PiperOrigin-RevId: 456362708

--- paxml/main.py
+++ paxml/main.py
@@ -22,6 +22,7 @@ $ bazel run -c opt --define=pax_task=lm \
     --job_log_dir=/tmp/jax_log_dir/exp01 --alsologtostderr
 """
 
+import importlib
 import os
 import random
 import re
@@ -145,9 +146,20 @@ def _get_experiment(experiment_name: str) -> base_experiment.BaseExperimentT:
   except:  # pylint: disable=bare-except
     pass
   experiment_class = experiment_registry.get(experiment_name)
-  if experiment_class is None:
-    raise ValueError(f'Could not find experiment `{experiment_name}`.')
-  return experiment_class
+  if experiment_class is not None:
+    return experiment_class
+  # Try to import the module that registers the experiment, assuming the
+  # experiment name contains the full path.
+  module_name = experiment_name.rsplit('.', 1)[0]
+  # internal experiment module import code
+  try:
+    importlib.import_module(module_name)
+  except ModuleNotFoundError as e:
+    raise ValueError(f'Could not find experiment `{experiment_name}`: {e}.')
+  experiment_class = experiment_registry.get(experiment_name)
+  if experiment_class is not None:
+    return experiment_class
+  raise ValueError(f'Could not find experiment `{experiment_name}`.')
 
 
 def wait_with_random_jitter(min_secs: int, max_secs: int) -> None:
