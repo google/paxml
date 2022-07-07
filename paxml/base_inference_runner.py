377ae613cb65819b0692eca7d17489bfe1dd641c
Jin Young Sohn <jysohn@google.com>
2022-07-07 07:48:06 -0700

    Introduce `BaseInferenceRunner` to generate output from a model with a schema
    PiperOrigin-RevId: 459511698

---
+++ paxml/base_inference_runner.py
@@ -0,0 +1,83 @@
+# coding=utf-8
+# Copyright 2022 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Base API for inference runners."""
+
+from __future__ import annotations
+
+import abc
+from typing import List
+
+from praxis import base_hyperparams
+from praxis import base_model
+from praxis import py_utils
+from praxis import pytypes
+from praxis import train_states
+import tensorflow_datasets as tfds
+
+NestedMap = py_utils.NestedMap
+PRNGKey = pytypes.PRNGKey
+TrainState = train_states.TrainState
+
+
+class BaseInferenceRunner(base_hyperparams.BaseParameterizable, abc.ABC):
+  """Abstract base class for users to override.
+
+  This class is essentially a container for a functional infer method and
+  output schema definition. It defines (1) how to run inference to generate
+  outputs given a model and some inputs, and (2) the corresponding schema for
+  the output.
+  """
+
+  def __init__(self, hparams: BaseInferenceRunner.HParams,
+               model: base_model.BaseModel) -> None:
+    super().__init__(hparams)
+    self._model = model
+
+  @abc.abstractmethod
+  def infer(self, train_state: TrainState, prng_key: PRNGKey,
+            input_batch: NestedMap) -> NestedMap:
+    """Generates some output given a model and input. Should be pmap-able."""
+
+  @property
+  @abc.abstractmethod
+  def output_schema(self) -> NestedMap:
+    """Returns the schema for the output to be serialized.
+
+    This must be a nested map of `tfds.features.FeatureConnector` types. See
+    https://www.tensorflow.org/datasets/api_docs/python/tfds/features/FeatureConnector
+    for more information. The following is an example:
+
+    ```
+    return NestedMap(
+        bucket_keys=tfds.features.Scalar(dtype=tf.int32),
+        nested=NestedMap(
+            field=tfds.features.Tensor(shape=(1000,), dtype=tf.int32)
+        ),
+        logprobs=tfds.features.Tensor(shape=(1, 32,), dtype=tf.float32),
+    )
+    ```
+    """
+
+  def serialize_outputs(self, outputs: NestedMap) -> List[bytes]:
+    input_batch_dim = 0
+    features_dict = tfds.features.FeaturesDict(self.output_schema)
+    examples = py_utils.tree_unstack(outputs, input_batch_dim)
+
+    serialized_examples = []
+    for ex in examples:
+      serialized_examples.append(features_dict.serialize_example(ex))
+
+    return serialized_examples
---
+++ paxml/base_inference_runner_test.py
@@ -0,0 +1,89 @@
+# coding=utf-8
+# Copyright 2022 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for base_inference_runner."""
+
+from __future__ import annotations
+
+from typing import Any, List
+
+from absl.testing import absltest
+import jax
+import numpy as np
+from paxml import base_inference_runner
+from praxis import base_hyperparams
+from praxis import base_model
+from praxis import py_utils
+from praxis import pytypes
+from praxis import test_utils
+from praxis import train_states
+import tensorflow.compat.v2 as tf
+import tensorflow_datasets as tfds
+
+instantiate = base_hyperparams.instantiate
+NestedMap = py_utils.NestedMap
+PRNGKey = pytypes.PRNGKey
+TrainState = train_states.TrainState
+
+
+class DummyInference(base_inference_runner.BaseInferenceRunner):
+
+  class HParams(base_inference_runner.BaseInferenceRunner.HParams):
+    output: Any = None
+    output_schema: Any = None
+
+  def infer(self, train_state: TrainState, prng_key: PRNGKey,
+            input_batch: NestedMap) -> NestedMap:
+    return self.hparams.output
+
+  @property
+  def output_schema(self) -> NestedMap:
+    return self.hparams.output_schema
+
+
+class BaseInferenceRunnerTest(test_utils.TestCase):
+
+  def test_infer(self):
+    dummy_output = NestedMap(
+        tensor=np.arange(64, dtype=np.float32).reshape(8, 8),
+        nested=NestedMap(
+            text=np.array([f'{i}'.encode('utf-8') for i in range(8)],
+                          dtype=object)))
+    dummy_schema = NestedMap(
+        tensor=tfds.features.Tensor(shape=(8,), dtype=tf.float32),
+        nested=NestedMap(text=tfds.features.Text()))
+
+    infer_runner_p = DummyInference.HParams(
+        output=dummy_output, output_schema=dummy_schema)
+    infer_runner = infer_runner_p.Instantiate(model=None)
+
+    serialized_outputs = infer_runner.serialize_outputs(
+        infer_runner.infer(*([None]*3)))
+
+    expected_outputs: List[NestedMap] = py_utils.tree_unstack(dummy_output, 0)
+    self.assertEqual(len(serialized_outputs), len(expected_outputs))
+
+    features_dict = tfds.features.FeaturesDict(dummy_schema)
+    for serialized, expected in zip(serialized_outputs, expected_outputs):
+      output = features_dict.deserialize_example(serialized)
+      output_np = jax.tree_map(lambda x: x.numpy(), output)
+
+      for output_leaf, expected_leaf in zip(
+          jax.tree_leaves(output_np), jax.tree_leaves(expected)):
+        self.assertArraysEqual(output_leaf, expected_leaf)
+
+
+if __name__ == '__main__':
+  absltest.main()
