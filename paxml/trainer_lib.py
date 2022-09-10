b058c659b613c491be56eee60b268ea71a2b6b80
Jin Young Sohn <jysohn@google.com>
2022-09-10 10:15:23 -0700

    Add verbosity to summaries and lower verbosity from eval / decode jobs
    PiperOrigin-RevId: 473475834

--- paxml/trainer_lib.py
+++ paxml/trainer_lib.py
@@ -738,7 +738,8 @@ def _eval_step_single_learner_with_model(
     summary_tensors - A nested map or dict of summary tensors computed in
       forward as well as backward pass.
   """
-  context_p = base_layer.JaxContext.HParams(do_eval=True)
+  context_p = base_layer.JaxContext.HParams(
+      do_eval=True, summary_verbosity=base_layer.SummaryVerbosity.INFO)
   # Fold in global_step as part of the random seed key, so that random
   # numbers depends on global step.
   prng_key = jax.random.fold_in(prng_key, states.step)
@@ -829,7 +830,8 @@ def decode_step(
     A tuple of (weighted_scalars, results, eval_metrics) as computed
       by model.decode() and the updated weights.
   """
-  context_p = base_layer.JaxContext.HParams(do_eval=True)
+  context_p = base_layer.JaxContext.HParams(
+      do_eval=True, summary_verbosity=base_layer.SummaryVerbosity.INFO)
   # Fold in global_step as part of the random seed key, so that random
   # numbers depends on global step.
   prng_key = jax.random.fold_in(prng_key, states.step)
