d3bbdfc660e2b365872455d5119decc599715f69
pax authors <no-reply@google.com>
2022-11-14 11:40:36 -0800

    Praxis 0.2.0 and Paxml 0.2.0 release preparation
    PiperOrigin-RevId: 488425729

---
+++ RELEASE.md
@@ -0,0 +1,19 @@
+# Version: 0.2.0
+## Major Features and Improvements
+## Breaking changes
+## Deprecations
+## Note
+*   Version: 0.2.0
+*   Build Date: 20221114
+*   Paxml commit: 16fd2143827522acfb7a7e22767a008eaae07e24
+*   Praxis version: 0.2.0
+*   Praxis commit: 413da1ad8148f27faebca119f8c5deedca66228b
+# Version: 0.1.0
+## Major Features and Improvements
+## Breaking changes
+## Deprecations
+## Note
+*   Version: 0.1.0
+*   Build Date: 20220702
+*   Commit: 546370f5323ef8b27d38ddc32445d7d3d1e4da9a
+*   Praxis version: 0.1.0
---
+++ paxml/pip_package/prepare_release.sh
@@ -0,0 +1,91 @@
+# coding=utf-8
+# Copyright 2022 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/bin/bash
+
+# This script prepare a new release by:
+# 1) update version number in setup.py and cloudbuild-release.yaml
+# 2) add a new section in RELEASE.md with version and corresponding commit
+
+set -e -x
+
+function print_help_and_exit {
+ echo "Usage: prepare_release.sh -v <paxml_version> -x <praxis_version> -d <build_date:YYYYMMDD> "
+ echo "exp: bash prepare_release.sh -v 0.2.0 -x 0.2.0 -d 20221114"
+ exit 0
+}
+
+while getopts "hv:d:x:" opt; do
+  case $opt in
+    v)
+      PAXML_VERSION=${OPTARG}
+      ;;
+    x)
+      PRAXIS_VERSION=${OPTARG}
+      ;;
+    d)
+      BUILD_DATE=${OPTARG}
+      ;;
+    *)
+      print_help_and_exit
+      ;;
+  esac
+done
+
+RELEASE_NOTE="../RELEASE.md"
+RELEASE_NOTE_NEW="release_new.md"
+
+if [[ -z "$BUILD_DATE" ]]; then
+  echo "Build date is required!"
+  exit 1
+fi
+
+if [[ -z "$PAXML_VERSION" ]]; then
+  echo "paxml version is required!"
+  exit 1
+fi
+
+echo "Build date: "$BUILD_DATE
+echo "PAXML version: "$PAXML_VERSION
+
+if [[ ! -z "$PRAXIS_VERSION" ]]; then
+    sed -i "s/_PRAXIS_VERSION: '[0-9.]*'/_PRAXIS_VERSION: '$PRAXIS_VERSION'/" cloudbuild-release.yaml
+fi
+
+sed -i "s/version='[0-9.]*'/version='$PAXML_VERSION'/" setup.py
+sed -i "s/_RELEASE_VERSION: '[0-9.]*'/_RELEASE_VERSION: '$PAXML_VERSION'/" cloudbuild-release.yaml
+gsutil cp gs://pax-on-cloud-tpu-project/wheels/"$BUILD_DATE"/paxml_commit.txt ./
+gsutil cp gs://pax-on-cloud-tpu-project/wheels/"$BUILD_DATE"/praxis_commit.txt ./
+PAXML_COMMIT=$(<paxml_commit.txt)
+PRAXIS_COMMIT=$(<praxis_commit.txt)
+rm paxml_commit.txt
+rm praxis_commit.txt
+echo "PAXML_COMMIT: " $PAXML_COMMIT
+[ -e $RELEASE_NOTE_NEW ] && rm $RELEASE_NOTE_NEW
+echo "# Version: $PAXML_VERSION" >> $RELEASE_NOTE_NEW
+echo "## Major Features and Improvements" >> $RELEASE_NOTE_NEW
+echo "## Breaking changes" >> $RELEASE_NOTE_NEW
+echo "## Deprecations" >> $RELEASE_NOTE_NEW
+echo "## Note" >> $RELEASE_NOTE_NEW
+echo "*   Version: $PAXML_VERSION" >> $RELEASE_NOTE_NEW
+echo "*   Build Date: $BUILD_DATE" >> $RELEASE_NOTE_NEW
+echo "*   Paxml commit: $PAXML_COMMIT" >> $RELEASE_NOTE_NEW
+echo "*   Praxis version: $PRAXIS_VERSION" >> $RELEASE_NOTE_NEW
+echo "*   Praxis commit: $PRAXIS_COMMIT" >> $RELEASE_NOTE_NEW
+RELEASE_NOTE_TMP="RELEASE.tmp.md"
+cat $RELEASE_NOTE_NEW $RELEASE_NOTE >> $RELEASE_NOTE_TMP
+rm $RELEASE_NOTE_NEW
+rm $RELEASE_NOTE
+mv $RELEASE_NOTE_TMP $RELEASE_NOTE
