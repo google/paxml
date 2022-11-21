12da06bfd30b17766febe4cdff5e1154ba341458
pax authors <no-reply@google.com>
2022-11-21 11:14:12 -0800

    praxis and paxml 0.2.1 release
    PiperOrigin-RevId: 490026727

--- paxml/pip_package/setup.py
+++ paxml/pip_package/setup.py
@@ -39,7 +39,7 @@ install_requires = _get_requirements()
 
 setup(
     name='paxml',
-    version='0.2.0',  # use major/minor version number, e.g. "0.1.0"
+    version='0.2.1',  # use major/minor version number, e.g. "0.1.0"
     description=('Framework to configure and run machine learning experiments '
                  'on top of Jax.'),
     author='PAX team',
