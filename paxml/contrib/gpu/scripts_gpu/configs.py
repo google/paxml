52a7fb5135a1b7c1998c6f4464e79f91de3b9535
ashors1 <ashors@nvidia.com>
2023-03-06 10:56:22 -0800

    freeze version of 'configure_gpt3_task'

--- paxml/contrib/gpu/scripts_gpu/configs.py
+++ paxml/contrib/gpu/scripts_gpu/configs.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 import jax.numpy as jnp
-from paxml.tasks.lm.params.c4 import configure_gpt3_task, TransformerLmSpmdAdam, C4SpmdPipelineGpt3AdamOrgHP
+from paxml.tasks.lm.params.c4 import TransformerLmSpmdAdam, C4SpmdPipelineGpt3AdamOrgHP
 from paxml.contrib.gpu.scripts_gpu.tasks import LambadaDataset, PileUnsupervisedDataset
 from paxml import experiment_registry
 from paxml import tasks_lib
@@ -25,6 +25,66 @@ from praxis import schedules
 from praxis.layers import transformers
 
 WeightInit = base_layer.WeightInit
+
+GPT_EOS_ID = 1
+
+## from https://github.com/google/paxml/commit/9b5682019806dcb058b82ec2f122aa30ed51f255
+def configure_gpt3_task(
+    cls,
+    task_p: tasks_lib.SingleTask.HParams,
+) -> tasks_lib.SingleTask.HParams:
+  """Returns task with gpt3 related configs."""
+  model_p = task_p.model  # pytype: disable=attribute-error  # enable-nested-classes
+
+  model_p.decoder_tpl.eos_id = (
+      GPT_EOS_ID  # pytype: disable=attribute-error  # enable-nested-classes
+  )
+  model_p.decoder_tpl.seqlen = cls.MAX_SEQ_LEN  # pytype: disable=attribute-error  # enable-nested-classes
+
+  model_p.params_init = WeightInit.Gaussian(0.006)
+
+  softmax_init = WeightInit.Gaussian(0.006)
+  model_p.lm_tpl.softmax_tpl.params_init = softmax_init
+  model_p.lm_tpl.softmax_tpl.feed_forward_tpl.has_bias = False
+  model_p.lm_tpl.softmax_tpl.soft_cap_logits = None
+
+  if cls.SEPARATE_EMBEDDING:
+    model_p.lm_tpl.separate_embedding_tpl.scale_sqrt_depth = False
+    model_p.lm_tpl.separate_embedding_tpl.lookup_style = (
+        cls.EMBEDDING_LOOKUP_STYLE
+    )
+  else:
+    model_p.lm_tpl.softmax_tpl.scale_sqrt_depth = False
+    model_p.lm_tpl.softmax_tpl.lookup_style = cls.EMBEDDING_LOOKUP_STYLE
+  if cls.TRAINABLE_POSITION_EMB:
+    model_p.lm_tpl.position_emb_tpl.lookup_style = cls.EMBEDDING_LOOKUP_STYLE
+
+  stacked_p = model_p.lm_tpl.stacked_transformer_tpl
+  if stacked_p.cls == transformers.PipelinedTransformer:
+    stacked_p = stacked_p.pipeline_stage
+  if issubclass(stacked_p.cls, transformers.StackedTransformerRepeated):
+    stacked_p = stacked_p.block
+  transformer_layer_p = stacked_p.transformer_layer_params_tpl
+
+  transformer_layer_p.ln_tpl.epsilon = cls.LAYERNORM_EPSILON
+  transformer_layer_p.tr_fflayer_tpl.ln_tpl.epsilon = cls.LAYERNORM_EPSILON
+  model_p.lm_tpl.final_ln_tpl.epsilon = cls.LAYERNORM_EPSILON
+  transformer_layer_p.tr_atten_tpl.internal_enable_per_dim_scale = False
+  transformer_layer_p.tr_atten_tpl.use_bias = True
+
+  transformer_layer_p.tr_fflayer_tpl.activation_tpl.approximate = True
+
+  for atten_p in (
+      transformer_layer_p.tr_atten_tpl,
+      transformer_layer_p.cross_atten_tpl,
+  ):
+    if atten_p is None:
+      continue
+    atten_wp = atten_p.weight_split_dims_mapping
+    atten_wp.proj = ['data', 'mdl', None]
+
+  return task_p
+
 
 ## 8 node
 @experiment_registry.register
@@ -180,5 +240,5 @@ class GPT175B(C4SpmdPipelineGpt3AdamOrgHP):
 @experiment_registry.register
 class SmallPileTest(Pile126M):
   """Base config for an SPMD model."""
-  ICI_MESH_SHAPE = [8,1,1]
+  ICI_MESH_SHAPE = [2,1,1]
     
