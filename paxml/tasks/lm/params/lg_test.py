5b17fadcc428d8bbe07023903f6718907cab6ae8
A9isha <anisha.mazumder@gmail.com>
2023-05-20 00:09:34 +0000

    update paths for train and validation data

--- paxml/tasks/lm/params/lg_test.py
+++ paxml/tasks/lm/params/lg_test.py
@@ -31,7 +31,7 @@ class LGSyntheticDataset(base_experiment.BaseExperiment):
       # TODO(zhangqiaorjc): Is this batch size too big for test?
       input_p.batch_size = batch_size
     input_p.seq_len = self.MAX_SEQ_LEN
-    input_p.file_pattern="tfrecord:gs://yejingxin-us-central2/external/lg/dummy-data/valid/*.tfrecords"
+    input_p.file_pattern="tfrecord:gs://yejingxin-us-central2/external/lg/dummy-data/train/*.tfrecords"
     input_p.file_parallelism = 16
     input_p.file_buffer_size = 16  # janghoon.han
     input_p.num_batcher_threads = 16
@@ -53,7 +53,7 @@ class LGSyntheticDataset(base_experiment.BaseExperiment):
       # TODO(zhangqiaorjc): Is this batch size too big for test?
       input_p.batch_size = batch_size
     input_p.seq_len = self.MAX_SEQ_LEN
-    input_p.file_pattern="tfrecord:gs://yejingxin-us-central2/external/lg/dummy-data/valid/*.tfrecords"
+    input_p.file_pattern="tfrecord:gs://yejingxin-us-central2/external/lg/dummy-data/eval/*.tfrecords"
     input_p.file_parallelism = 1
     input_p.file_buffer_size = 1  # janghoon.han
     input_p.file_random_seed =0
