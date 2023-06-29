77703efc2c2a460f82bf2e508313d3474f159e1b
The paxml Authors <no-reply@google.com>
2023-06-29 10:35:17 -0700

    Implements a wrapped version of TransformerLm that is compatible with ghost clipping
    - Adds generate_wrapped_template function that can be used to wrap pax model templates.
    - Adds unit test to test the generated TransformerLm model config.
    
    PiperOrigin-RevId: 544397585

--- paxml/ghostnorm/generic_wrapper.py
+++ paxml/ghostnorm/generic_wrapper.py
@@ -20,15 +20,23 @@ from typing import Any, Callable
 import jax
 import jax.numpy as jnp
 from paxml.ghostnorm import base
+from paxml.ghostnorm import embedding
+from paxml.ghostnorm import linears
 from praxis import base_layer
 from praxis import pax_fiddle
 from praxis import pytypes
+from praxis.layers import attentions as praxis_attentions
+from praxis.layers import embedding_softmax as praxis_embedding
+from praxis.layers import linears as praxis_linears
+from praxis.layers import normalizations as praxis_normalizations
+from praxis.layers import transformers as praxis_transformers
+
 
 template_field = base_layer.template_field
 instantiate = base_layer.instantiate
 LayerTpl = pax_fiddle.Config[base_layer.BaseLayer]
 PARAMS = base_layer.PARAMS
-LAYER = 'layer'
+LAYER = 'ghostnorm_wrapped_layer'
 JTensor = pytypes.JTensor
 NestedJTensor = pytypes.NestedJTensor
 ResType = tuple[
@@ -122,11 +130,102 @@ class WrappedGhostNorm(base_layer.BaseLayer):
     super().setup()
     if self.layer_tpl is not None:
       self.create_child(LAYER, self.layer_tpl.clone())
-    self.layer_fn = _create_ghostnorm_fn(self.layer.apply)
+    self.layer_fn = _create_ghostnorm_fn(self.ghostnorm_wrapped_layer.apply)
 
   def __call__(self, *args: Any) -> JTensor:
     # This is a special case that is used when the layer is being initialized.
     if PARAMS not in self.variables:
-      return self.layer(*args)
+      return self.ghostnorm_wrapped_layer(*args)
 
     return self.layer_fn({PARAMS: self.variables[PARAMS][LAYER]}, *args)
+
+
+_SPECIAL_ATTRS = {'layer_tpl'}
+
+
+class GhostNormPaxConfig(pax_fiddle.Config):
+  """A special PaxFiddle config which applies attributes to its child."""
+
+  def set(self, *args, **kwargs):
+    self.layer_tpl.set(*args, **kwargs)
+    return self
+
+  def __getattr__(self, name: str) -> Any:
+    if name in _SPECIAL_ATTRS:
+      return super().__getattr__(name)
+    return self.layer_tpl.__getattr__(name)
+
+  def __setattr__(self, name: str, value: Any):
+    if name in _SPECIAL_ATTRS:
+      super().__setattr__(name, value)
+    else:
+      self.layer_tpl.__setattr__(name, value)
+
+
+# Add layers to this list that should be wrapped with WrappedGhostNorm wrapper.
+# Note that this list should mututally exclusive with _REPLACE_MAP.
+_WRAPPABLE_LAYERS = {
+    praxis_normalizations.LayerNorm,
+    praxis_attentions.PerDimScale,
+    praxis_attentions.CausalDepthwiseConv1D,
+    praxis_attentions.AttentionProjection,  # optimize most likely.
+    praxis_attentions.CombinedQKVProjectionLayer,  # optimize most likely.
+    praxis_transformers.TransformerFeedForwardMoe,  # optimize maybe.
+}
+
+# Add a mapping to replace layer with a custom implementation.
+_REPLACE_MAP = {
+    praxis_embedding.Embedding: embedding.EmbeddingGhostNorm,
+    praxis_linears.Linear: linears.LinearGhostNorm,
+    praxis_linears.Bias: linears.BiasGhostNorm,
+}
+
+
+def _is_wrappable(model_or_layer_p: pax_fiddle.Config) -> bool:
+  return (
+      issubclass(model_or_layer_p.cls, base_layer.BaseLayer)
+      and model_or_layer_p.cls in _WRAPPABLE_LAYERS
+  )
+
+
+def _is_replaceable(model_or_layer_p: pax_fiddle.Config) -> bool:
+  return model_or_layer_p.cls in _REPLACE_MAP.keys()
+
+
+def _replace(model_or_layer_p: pax_fiddle.Config) -> pax_fiddle.Config:
+  model_or_layer_p.cls = _REPLACE_MAP[model_or_layer_p.cls]
+  return model_or_layer_p
+
+
+def generate_wrapped_template(
+    model_or_layer_p: pax_fiddle.Config,
+) -> pax_fiddle.Config:
+  """Wraps a Pax Layer PaxFiddle template to be compatible with ghost clipping.
+
+  Note that it only replaces or wraps layers that we know are compatible. To be
+  compatible, all the parameteric layers (layers with trainable parameters) in
+  the model/layer need to either wrappable or replacable. Furthermore, weight
+  sharing is not allowed and will cause an error even if all the others can be
+  wrapped or replaced.
+
+  Args:
+    model_or_layer_p: A PaxConfig describing the model or layer to wrap.
+
+  Returns:
+    A PaxConfig describing the wrapped model.
+  """
+  assert isinstance(model_or_layer_p, pax_fiddle.Config)
+  for attr_name in model_or_layer_p.__dir__():
+    layer_p = model_or_layer_p.__getattr__(attr_name)
+    if isinstance(layer_p, pax_fiddle.Config) and issubclass(
+        layer_p.cls, base_layer.BaseLayer
+    ):
+      wrapped_layer_p = generate_wrapped_template(layer_p)
+      model_or_layer_p.__setattr__(attr_name, wrapped_layer_p)
+  if _is_replaceable(model_or_layer_p):
+    model_or_layer_p = _replace(model_or_layer_p)
+  if _is_wrappable(model_or_layer_p):
+    model_or_layer_p = GhostNormPaxConfig(
+        WrappedGhostNorm, layer_tpl=model_or_layer_p
+    )
+  return model_or_layer_p
--- paxml/ghostnorm/layers_test.py
+++ paxml/ghostnorm/layers_test.py
@@ -14,7 +14,8 @@
 # limitations under the License.
 
 """Parameteric tests for layers that implemented the ghost norm protocol."""
-
+import copy
+import functools
 from absl.testing import absltest
 from absl.testing import parameterized
 import jax
@@ -27,8 +28,10 @@ from paxml.ghostnorm import generic_wrapper
 from paxml.ghostnorm import linears
 from praxis import base_layer
 from praxis import pax_fiddle
+from praxis import py_utils
 from praxis.layers import embedding_softmax as praxis_embedding
 from praxis.layers import linears as praxis_linears
+from praxis.layers import transformer_models as praxis_transformer_models
 import tensorflow as tf
 
 instantiate = base_layer.instantiate
@@ -47,6 +50,17 @@ def mean_loss_for_testing(outputs):
   )
 
 
+def _remove_generic_layer_structure(pytree):
+  if isinstance(pytree, dict):
+    if LAYER in pytree:
+      if len(pytree.keys()) != 1:
+        raise ValueError('Unsupported pytree structure.')
+      return pytree[LAYER]
+    for key in pytree:
+      pytree[key] = _remove_generic_layer_structure(pytree[key])
+  return pytree
+
+
 class LayersTest(parameterized.TestCase):
 
   def setUp(self):
@@ -54,35 +68,54 @@ class LayersTest(parameterized.TestCase):
     np.random.seed(123456)
     tf.random.set_seed(123)
 
-  def _get_loss_fns(self, ghostnorm_layer, ref_layer):
+  def _get_loss_fns(self, ghostnorm_layer, ref_layer, extract_outputs_fn=None):
     noise_key = jax.random.PRNGKey(seed=12345)
 
-    def loss_fn(mdl_vars, inputs):
+    def loss_fn(mdl_vars, *inputs_args, **inputs_kwargs):
       outputs = ghostnorm_layer.apply(
-          mdl_vars, inputs, rngs={RANDOM: noise_key}
+          mdl_vars, *inputs_args, **inputs_kwargs, rngs={RANDOM: noise_key}
       )
+      if extract_outputs_fn:
+        outputs = extract_outputs_fn(outputs)
       return mean_loss_for_testing(outputs)
 
-    def loss_fn_ref(mdl_vars, inputs):
-      outputs = ref_layer.apply(mdl_vars, inputs, rngs={RANDOM: noise_key})
+    def loss_fn_ref(mdl_vars, *inputs_args, **inputs_kwargs):
+      outputs = ref_layer.apply(
+          mdl_vars, *inputs_args, **inputs_kwargs, rngs={RANDOM: noise_key}
+      )
+      if extract_outputs_fn:
+        outputs = extract_outputs_fn(outputs)
       return mean_loss_for_testing(outputs)
 
     return loss_fn, loss_fn_ref
 
-  def _get_per_eg_grad(self, inputs, initial_vars, loss_fn):
+  def _get_per_eg_grad(
+      self, initial_vars, loss_fn, *inputs_args, **inputs_kwargs
+  ):
     # per-example gradients with jax.vmap
-    per_eg_grad_fn = jax.vmap(jax.grad(loss_fn), in_axes=(None, 0))
-    vmap_inputs = jax.tree_map(lambda x: jnp.expand_dims(x, axis=1), inputs)
-    per_eg_grad = per_eg_grad_fn(initial_vars, vmap_inputs)
+    grad_fn = jax.grad(loss_fn)
+    grad_fn_with_vars = functools.partial(grad_fn, initial_vars)
+    per_eg_grad_fn = jax.vmap(grad_fn_with_vars)
+    vmap_inputs_args = jax.tree_map(
+        lambda x: jnp.expand_dims(x, axis=1), inputs_args
+    )
+    vmap_inputs_kwargs = jax.tree_map(
+        lambda x: jnp.expand_dims(x, axis=1), inputs_kwargs
+    )
+    per_eg_grad = per_eg_grad_fn(*vmap_inputs_args, **vmap_inputs_kwargs)
     return per_eg_grad
 
-  def _get_grad_and_norms(self, inputs, initial_vars, loss_fn, scales):
+  def _get_grad_and_norms(
+      self, initial_vars, loss_fn, scales, *inputs_args, **inputs_kwargs
+  ):
     grad_fn = jax.grad(loss_fn)
     params_with_sq_norms = jax.tree_map(
         lambda x: base.ParamWithAux(x, scales), initial_vars[PARAMS]
     )
     params_with_sq_norms = {**initial_vars, PARAMS: params_with_sq_norms}
-    grad_with_sq_norms = grad_fn(params_with_sq_norms, inputs)[PARAMS]
+    grad_with_sq_norms = grad_fn(
+        params_with_sq_norms, *inputs_args, **inputs_kwargs
+    )[PARAMS]
 
     is_leaf = lambda x: isinstance(x, base.ParamWithAux)
     fast_per_eg_grad_norms = jnp.sqrt(
@@ -97,21 +130,29 @@ class LayersTest(parameterized.TestCase):
 
   def assertOutputsAreSame(
       self,
-      inputs,
       ghostnorm_initial_vars,
       initial_vars,
       ghostnorm_layer,
       ref_layer,
+      *input_args,
+      **input_kwargs,
   ):
+    extract_outputs_fn = input_kwargs.pop('extract_outputs_fn', None)
     noise_key = jax.random.PRNGKey(seed=12345)
 
     # make sure the layer behaves the same as the reference layer in forward
     ghostnorm_outputs = ghostnorm_layer.apply(
-        ghostnorm_initial_vars, inputs, rngs={RANDOM: noise_key}
+        ghostnorm_initial_vars,
+        *input_args,
+        **input_kwargs,
+        rngs={RANDOM: noise_key},
     )
     ref_outputs = ref_layer.apply(
-        initial_vars, inputs, rngs={RANDOM: noise_key}
+        initial_vars, *input_args, **input_kwargs, rngs={RANDOM: noise_key}
     )
+    if extract_outputs_fn is not None:
+      ghostnorm_outputs = extract_outputs_fn(ghostnorm_outputs)
+      ref_outputs = extract_outputs_fn(ref_outputs)
     np.testing.assert_allclose(
         ghostnorm_outputs,
         ref_outputs,
@@ -120,14 +161,23 @@ class LayersTest(parameterized.TestCase):
         err_msg='outputs are different.',
     )
 
-  def assertGradNormsAreSame(self, inputs, initial_vars, loss_fn):
+  def assertGradNormsAreSame(
+      self, initial_vars, loss_fn, *inputs_args, **inputs_kwargs
+  ):
     # get expected per-example gradient norms by explicitly materialize
-    per_eg_grad = self._get_per_eg_grad(inputs, initial_vars, loss_fn)
+    per_eg_grad = self._get_per_eg_grad(
+        initial_vars, loss_fn, *inputs_args, **inputs_kwargs
+    )
     per_eg_grad_norms = jax.vmap(optax.global_norm)(per_eg_grad)
 
     # first pass to compute gradient norm
+    batch_size = inputs_args[0].shape[0]
     _, fast_per_eg_grad_norms = self._get_grad_and_norms(
-        inputs, initial_vars, loss_fn, jnp.ones(inputs.shape[0])
+        initial_vars,
+        loss_fn,
+        jnp.ones(batch_size),
+        *inputs_args,
+        **inputs_kwargs,
     )
 
     # test if the computed per-example gradient norms match expected values
@@ -143,25 +193,28 @@ class LayersTest(parameterized.TestCase):
 
   def assertGradIsSame(
       self,
-      inputs,
       ghost_initial_vars,
       initial_vars,
       loss_fn,
       loss_fn_ref,
-      unwrap_grad=False,
+      *inputs_args,
+      **inputs_kwargs,
   ):
+    unwrap_grad = inputs_kwargs.pop('unwrap_grad', False)
+    batch_size = inputs_args[0].shape[0]
     grad_with_sq_norms, _ = self._get_grad_and_norms(
-        inputs,
         ghost_initial_vars,
         loss_fn,
-        jnp.ones(inputs.shape[0]),
+        jnp.ones(batch_size),
+        *inputs_args,
+        **inputs_kwargs,
     )
     if unwrap_grad:
-      grad_with_sq_norms = grad_with_sq_norms[LAYER]
+      grad_with_sq_norms = _remove_generic_layer_structure(grad_with_sq_norms)
 
     # test if the computed gradient matches the grad of the reference layer
     grad_fn_ref = jax.grad(loss_fn_ref)
-    grad_ref = grad_fn_ref(initial_vars, inputs)[PARAMS]
+    grad_ref = grad_fn_ref(initial_vars, *inputs_args, **inputs_kwargs)[PARAMS]
     grad_diffs = jax.tree_map(
         lambda x, y: np.mean(np.abs(x - y.param)), grad_ref, grad_with_sq_norms
     )
@@ -173,17 +226,24 @@ class LayersTest(parameterized.TestCase):
         err_msg='average gradients are different.',
     )
 
-  def assertGradIsClippedToScale(self, inputs, initial_vars, loss_fn):
+  def assertGradIsClippedToScale(
+      self, initial_vars, loss_fn, *inputs_args, **inputs_kwargs
+  ):
     # first pass to compute gradient norm
+    batch_size = inputs_args[0].shape[0]
     _, fast_per_eg_grad_norms = self._get_grad_and_norms(
-        inputs, initial_vars, loss_fn, jnp.ones(inputs.shape[0])
+        initial_vars,
+        loss_fn,
+        jnp.ones(batch_size),
+        *inputs_args,
+        **inputs_kwargs,
     )
 
     # second pass to compute norm-clipped gradients
     l2_clip = 0.2
     scales = jnp.minimum(1.0, l2_clip / fast_per_eg_grad_norms)
     _, scaled_fast_per_eg_grad_norms = self._get_grad_and_norms(
-        inputs, initial_vars, loss_fn, scales
+        initial_vars, loss_fn, scales, *inputs_args, **inputs_kwargs
     )
 
     self.assertTrue(
@@ -191,10 +251,15 @@ class LayersTest(parameterized.TestCase):
         msg='norm clipping conditions are not satisfied.',
     )
 
-  def assertClippedGradEqualsScaledGrad(self, inputs, initial_vars, loss_fn):
+  def assertClippedGradEqualsScaledGrad(
+      self, initial_vars, loss_fn, *inputs_args, **inputs_kwargs
+  ):
     l2_clip = 0.2
+    batch_size = inputs_args[0].shape[0]
     # get expected per-example gradient norms by explicitly materialize
-    per_eg_grad = self._get_per_eg_grad(inputs, initial_vars, loss_fn)
+    per_eg_grad = self._get_per_eg_grad(
+        initial_vars, loss_fn, *inputs_args, **inputs_kwargs
+    )
 
     # compute the expected average gradients from the clipped per-example grads
     grads_flat, grads_treedef = jax.tree_flatten(per_eg_grad[PARAMS])
@@ -202,17 +267,18 @@ class LayersTest(parameterized.TestCase):
         grads=grads_flat, l2_norm_clip=l2_clip
     )
     sum_grads = jax.tree_unflatten(grads_treedef, sum_clipped)
-    expected_grads = jax.tree_map(lambda x: x / inputs.shape[0], sum_grads)
+    expected_grads = jax.tree_map(lambda x: x / batch_size, sum_grads)
 
     _, fast_per_eg_grad_norms = self._get_grad_and_norms(
-        inputs, initial_vars, loss_fn, jnp.ones(inputs.shape[0])
+        initial_vars,
+        loss_fn,
+        jnp.ones(batch_size),
+        *inputs_args,
+        **inputs_kwargs,
     )
     scales = jnp.minimum(1.0, l2_clip / fast_per_eg_grad_norms)
     grad_with_sq_norms, _ = self._get_grad_and_norms(
-        inputs,
-        initial_vars,
-        loss_fn,
-        scales,
+        initial_vars, loss_fn, scales, *inputs_args, **inputs_kwargs
     )
 
     is_leaf = lambda x: isinstance(x, base.ParamWithAux)
@@ -227,7 +293,7 @@ class LayersTest(parameterized.TestCase):
         jax.tree_util.tree_flatten(diffs)[0],
         0,
         rtol=1e-5,
-        atol=1e-5,
+        atol=1e-2,
         err_msg='ghost norm clipping outputs incorrect average gradients.',
     )
 
@@ -331,17 +397,17 @@ class LayersTest(parameterized.TestCase):
     )
 
     self.assertOutputsAreSame(
-        inputs, initial_vars, initial_vars, ghostnorm_layer, ref_layer
+        initial_vars, initial_vars, ghostnorm_layer, ref_layer, inputs
     )
 
     loss_fn, loss_fn_ref = self._get_loss_fns(ghostnorm_layer, ref_layer)
 
-    self.assertGradNormsAreSame(inputs, initial_vars, loss_fn)
+    self.assertGradNormsAreSame(initial_vars, loss_fn, inputs)
     self.assertGradIsSame(
-        inputs, initial_vars, initial_vars, loss_fn, loss_fn_ref
+        initial_vars, initial_vars, loss_fn, loss_fn_ref, inputs
     )
-    self.assertGradIsClippedToScale(inputs, initial_vars, loss_fn)
-    self.assertClippedGradEqualsScaledGrad(inputs, initial_vars, loss_fn)
+    self.assertGradIsClippedToScale(initial_vars, loss_fn, inputs)
+    self.assertClippedGradEqualsScaledGrad(initial_vars, loss_fn, inputs)
 
   @parameterized.named_parameters(
       {
@@ -383,17 +449,151 @@ class LayersTest(parameterized.TestCase):
     initial_vars = {PARAMS: ghost_initial_vars[PARAMS][LAYER]}
 
     self.assertOutputsAreSame(
-        inputs, ghost_initial_vars, initial_vars, ghostnorm_layer, ref_layer
+        ghost_initial_vars, initial_vars, ghostnorm_layer, ref_layer, inputs
     )
 
     loss_fn, loss_fn_ref = self._get_loss_fns(ghostnorm_layer, ref_layer)
 
-    self.assertGradNormsAreSame(inputs, ghost_initial_vars, loss_fn)
+    self.assertGradNormsAreSame(ghost_initial_vars, loss_fn, inputs)
     self.assertGradIsSame(
-        inputs, ghost_initial_vars, initial_vars, loss_fn, loss_fn_ref, True
+        ghost_initial_vars,
+        initial_vars,
+        loss_fn,
+        loss_fn_ref,
+        inputs,
+        unwrap_grad=True,
     )
-    self.assertGradIsClippedToScale(inputs, ghost_initial_vars, loss_fn)
-    self.assertClippedGradEqualsScaledGrad(inputs, ghost_initial_vars, loss_fn)
+    self.assertGradIsClippedToScale(ghost_initial_vars, loss_fn, inputs)
+    self.assertClippedGradEqualsScaledGrad(ghost_initial_vars, loss_fn, inputs)
+
+  @parameterized.named_parameters(
+      {
+          'testcase_name': 'Causal',
+          'model_type': praxis_transformer_models.LanguageModelType.CAUSAL,
+      },
+      {
+          'testcase_name': 'Prefix',
+          'model_type': praxis_transformer_models.LanguageModelType.PREFIX,
+      },
+  )
+  def test_calculate_grad_norms_transformer(self, model_type):
+    batch_size = 2
+    seq_len = 10
+    model_dim = 32
+    vocab_size = 52
+    num_heads = 4
+    ref_layer_tpl = pax_fiddle.Config(
+        praxis_transformer_models.TransformerLm,
+        name='bert_lm',
+        model_type=model_type,
+        model_dims=model_dim,
+        vocab_size=vocab_size,
+        skip_compute_loss=False,
+    )
+
+    stacked_transformer_tpl = ref_layer_tpl.stacked_transformer_tpl
+    stacked_transformer_tpl.model_dims = model_dim
+    stacked_transformer_tpl.hidden_dims = num_heads * model_dim
+    stacked_transformer_tpl.num_heads = num_heads
+    stacked_transformer_tpl.num_layers = 1
+    ref_layer_tpl.softmax_tpl.scale_sqrt_depth = True
+
+    # Use a separate embedding as shared weights are not compatible with
+    # ghost clipping.
+    ref_layer_tpl.separate_embedding_tpl = pax_fiddle.Config(
+        praxis_embedding.Embedding
+    )
+
+    inputs = jax.random.randint(
+        jax.random.PRNGKey(1234), [batch_size, seq_len], 0, vocab_size
+    )
+    input_paddings = jnp.zeros([batch_size, seq_len])
+    input_weights = jnp.ones([batch_size, seq_len])
+    input_segment_ids = jnp.ones([batch_size, seq_len])
+    input_segment_pos = jnp.tile(
+        jnp.arange(0, seq_len)[jnp.newaxis, :], [batch_size, 1]
+    )
+    labels = py_utils.NestedMap()
+    labels.class_ids = inputs
+    labels.class_weights = input_weights
+
+    ghostnorm_layer_tpl = generic_wrapper.generate_wrapped_template(
+        ref_layer_tpl.clone()
+    )
+
+    ref_layer = instantiate(ref_layer_tpl)
+    ghostnorm_layer = instantiate(ghostnorm_layer_tpl)
+
+    ghost_initial_vars = ghostnorm_layer.init(
+        jax.random.PRNGKey(1234),
+        inputs,
+        paddings=input_paddings,
+        labels=labels,
+        segment_ids=input_segment_ids,
+        segment_pos=input_segment_pos,
+    )
+    initial_vars = _remove_generic_layer_structure(
+        copy.deepcopy(ghost_initial_vars)
+    )
+
+    extract_outputs_fn = lambda out: out.logits
+
+    self.assertOutputsAreSame(
+        ghost_initial_vars,
+        initial_vars,
+        ghostnorm_layer,
+        ref_layer,
+        inputs,
+        paddings=input_paddings,
+        labels=labels,
+        segment_ids=input_segment_ids,
+        segment_pos=input_segment_pos,
+        extract_outputs_fn=extract_outputs_fn,
+    )
+
+    loss_fn, loss_fn_ref = self._get_loss_fns(
+        ghostnorm_layer, ref_layer, extract_outputs_fn=extract_outputs_fn
+    )
+
+    self.assertGradNormsAreSame(
+        ghost_initial_vars,
+        loss_fn,
+        inputs,
+        paddings=input_paddings,
+        labels=labels,
+        segment_ids=input_segment_ids,
+        segment_pos=input_segment_pos,
+    )
+    self.assertGradIsSame(
+        ghost_initial_vars,
+        initial_vars,
+        loss_fn,
+        loss_fn_ref,
+        inputs,
+        paddings=input_paddings,
+        labels=labels,
+        segment_ids=input_segment_ids,
+        segment_pos=input_segment_pos,
+        unwrap_grad=True,
+    )
+    self.assertGradIsClippedToScale(
+        ghost_initial_vars,
+        loss_fn,
+        inputs,
+        paddings=input_paddings,
+        labels=labels,
+        segment_ids=input_segment_ids,
+        segment_pos=input_segment_pos,
+    )
+    self.assertClippedGradEqualsScaledGrad(
+        ghost_initial_vars,
+        loss_fn,
+        inputs,
+        paddings=input_paddings,
+        labels=labels,
+        segment_ids=input_segment_ids,
+        segment_pos=input_segment_pos,
+    )
 
 
 if __name__ == '__main__':
