Fix clipping bound scaling by using the reciprocal of the number of devices explicitly in pmap'ed DpSgdStochasticGradient and 1.0 otherwise.
PiperOrigin-RevId: 594512770

paxml/sgf.py
@@ -113,6 +113,39 @@ class StandardGradient(BaseStochasticGradient):
     return (values, aux), grads
 
 
+def _clipping_bound_scaling(
+    use_loss_weight_scaling: bool = False,
+    loss_weight: float | None = None,
+) -> float:
+  """Parse use_loss_weight_scaling to get the right scaling of clipping bound.
+
+  Args:
+    use_loss_weight_scaling: Whether to use loss_weight to scale the clipping
+      bound. If set to False, use 1. / jax.device_count() under pmap, or 1.0
+      otherwise for scaling. If set to True, use loss_weight.
+    loss_weight: The loss_weight used to scale the gradients.
+
+  Returns:
+    The scaling to apply to the clipping bound in float.
+  """
+  if use_loss_weight_scaling:
+    if loss_weight is None:
+      raise ValueError(
+          'loss_weight must be set when use_loss_weight_scaling is set to True.'
+      )
+    else:
+      return loss_weight
+  else:
+    if base_layer.is_running_under_pmap():
+      # TODO(b/315502275): The following line assumes the loss aggregator being
+      # used takes a weighted average of the loss. For example, the default
+      # loss aggregator /third_party/py/paxml/base_metrics.py;l=398-407. Should
+      # add support for customized scaling factor under pmap.
+      return 1.0 / jax.device_count()
+    else:
+      return 1.0
+
+
 class PercoreClippedDpSgdGradient(BaseStochasticGradient):
   """DP-SGD stochastic gradient function using per-core clipping.
 
@@ -132,18 +165,13 @@ class PercoreClippedDpSgdGradient(BaseStochasticGradient):
     l2_norm_clip: The L2 clipping bound used to clip per-core gradients.
     noise_multiplier: The noise multiplier used to decide the noise scale. See
       Section 5.3.2 of https://arxiv.org/pdf/2303.00654.pdf for more details.
-    clipping_bound_scaling: The scaling of the clipping bound to adjust to the
-      gradient scaling. When running under pmap, losses and gradients are scaled
-      using aux.loss_weight on each TPU core so clipping bound should be scaled
-      accordingly. Legal values include 1) `None` (default): Set
-      clipping_bound_scaling to 1. / jax.device_count() under pmap, or throws an
-      error otherwise. 2) 'use_aux_loss_weight': Set clipping_bound_scaling to
-      aux.loss_weight. Note for models like CtcModel, aux.loss_weight is
-      different across TPU cores, which can cause weaker privacy guarantee than
-      expected in differential privacy. We strongly recommend to only use this
-      option for empirical privacy. If used for differential privacy, ad hoc
-      care has to be taken when accounting privacy budget. 3) `float`: Manually
-      set the value for clipping_bound_scaling.
+    use_loss_weight_scaling: Whether to use aux.loss_weight to scale the
+      clipping bound. If set to False, use 1. / jax.device_count() under pmap,
+      or 1.0 otherwise for scaling. If set to True, use aux.loss_weight. Note
+      for models like CtcModel, loss_weight is different across TPU cores, which
+      can cause unexpected behavior for differential privacy so we only allow
+      this option to be turned on for empirical privacy with noise_multiplier =
+      0.0.
     normalize_gradients: Whether to apply Gradient Normalization as implemented
       in eqn 3 of https://arxiv.org/abs/2204.13650 to reduce the dependence
       between clipping value and learning rate. Note that normalization is only
@@ -156,7 +184,7 @@ class PercoreClippedDpSgdGradient(BaseStochasticGradient):
 
   l2_norm_clip: float = 0.0
   noise_multiplier: float = 0.0
-  clipping_bound_scaling: float | str | None = None
+  use_loss_weight_scaling: bool = False
   normalize_gradients: bool = False
   adaptive_clipping_method: str | None = None
 
@@ -223,6 +251,10 @@ class PercoreClippedDpSgdGradient(BaseStochasticGradient):
           'PercoreClippedDpSgdGradient is only supported when running under'
           ' Pmap Partitioning.  Please set ICI_MESH_SHAPE = None.'
       )
+    assert not (
+        self.use_loss_weight_scaling and self.noise_multiplier != 0.0
+    ), 'Noise multiplier must be 0.0 when use_loss_weight_scaling is True.'
+
     # Obtain the per-core mean gradient.
     grad_fn = jax.value_and_grad(loss_fn, has_aux=True, allow_int=True)
     (values, aux), grads = grad_fn(
@@ -230,26 +262,15 @@ class PercoreClippedDpSgdGradient(BaseStochasticGradient):
     )
     aux = self.process_aux_info(aux)
 
-    if self.clipping_bound_scaling is None:
-      if base_layer.is_running_under_pmap():
-        self.clipping_bound_scaling = 1.0 / jax.device_count()
-      else:
-        raise ValueError(
-            'clipping_bound_scaling must be set explicitly when'
-            'not running under pmap.'
-        )
-    elif self.clipping_bound_scaling == 'use_aux_loss_weight':
-      self.clipping_bound_scaling = aux.loss_weight
-    elif not isinstance(self.clipping_bound_scaling, float):
-      raise ValueError(
-          f'Unsupported clipping_bound_scaling: {self.clipping_bound_scaling}'
-      )
+    clipping_bound_scaling = _clipping_bound_scaling(
+        self.use_loss_weight_scaling, aux.loss_weight
+    )
 
     if self.adaptive_clipping_method == 'min':
       grads_norm = optax.global_norm(grads)
       self.l2_norm_clip = (
           jax.lax.pmin(grads_norm, axis_name=PMAP_PARALLEL_AXIS_NAME)
-          / self.clipping_bound_scaling
+          / clipping_bound_scaling
       )
     elif self.adaptive_clipping_method is not None:
       raise ValueError(
@@ -258,7 +279,7 @@ class PercoreClippedDpSgdGradient(BaseStochasticGradient):
       )
 
     grads, num_clipped, grad_norm = self._clip_gradients(
-        grads, self.clipping_bound_scaling * self.l2_norm_clip
+        grads, clipping_bound_scaling * self.l2_norm_clip
     )
 
     if self.normalize_gradients:
@@ -270,7 +291,7 @@ class PercoreClippedDpSgdGradient(BaseStochasticGradient):
           1.0 if self.normalize_gradients else self.l2_norm_clip
       )
       grads = self._add_noise(
-          grads, noise_stddev, self.clipping_bound_scaling, prng_key
+          grads, noise_stddev, clipping_bound_scaling, prng_key
       )
 
     return (
@@ -299,6 +320,13 @@ class DpSgdStochasticGradient(BaseStochasticGradient):
     l2_norm_clip: The L2 clipping bound used to clip per-core gradients.
     noise_multiplier: The noise multiplier used to decide the noise scale. See
       Section 5.3.2 of https://arxiv.org/pdf/2303.00654.pdf for more details.
+    use_loss_weight_scaling: Whether to use aux.loss_weight to scale the
+      clipping bound. If set to False, use 1. / jax.device_count() under pmap,
+      or 1.0 otherwise for scaling. If set to True, use aux.loss_weight. Note
+      for models like CtcModel, loss_weight is different across TPU cores, which
+      can cause unexpected behavior for differential privacy so we only allow
+      this option to be turned on for empirical privacy with noise_multiplier =
+      0.0.
     normalize_gradients: Whether to apply Gradient Normalization as implemented
       in eqn 3 of https://arxiv.org/abs/2204.13650 to reduce the dependence
       between clipping value and learning rate. Note that normalization is only
@@ -323,6 +351,7 @@ class DpSgdStochasticGradient(BaseStochasticGradient):
 
   l2_norm_clip: float = 0.0
   noise_multiplier: float = 0.0
+  use_loss_weight_scaling: bool = False
   normalize_gradients: bool = False
   inner_batch_size: int | None = None
 
@@ -356,7 +385,7 @@ class DpSgdStochasticGradient(BaseStochasticGradient):
       self,
       grads: NestedMap,
       noise_stddev: float,
-      loss_weight: float,
+      clipping_bound_scaling: float,
       prng_key: PRNGKey = None,
   ) -> NestedMap:
     prng_keys = jax.random.split(
@@ -367,14 +396,14 @@ class DpSgdStochasticGradient(BaseStochasticGradient):
     )
 
     if base_layer.is_running_under_pmap():
-      # Note: when running under pmap, loss_weight is set to 1/num_devices.
-      # In this case, the *global* batch size is batch_size / loss_weight.
+      # Note: Because `l2_norm_clip` is scaled with `clipping_bound_scaling`, we
+      # need to scale the noise_std accordingly to compensate for the change.
       # Moreover, each device adds independent Gaussian noises, and then the
       # noisy gradients are added with `psum``. Because the sum of num_devices
       # copies of independent Gaussian noises is equivalent to a single Gaussian
       # with std scaled by `sqrt(num_devices)``, we need to further scale the
       # noise_std on each device to correct this.
-      noise_stddev *= loss_weight * jnp.sqrt(loss_weight)
+      noise_stddev *= clipping_bound_scaling * jnp.sqrt(clipping_bound_scaling)
 
     def _add_noise_to_array(x, prng):
       return x + noise_stddev * jax.random.normal(prng, shape=x.shape)
@@ -400,6 +429,9 @@ class DpSgdStochasticGradient(BaseStochasticGradient):
     assert (
         self.l2_norm_clip > 0.0
     ), f'Clipping bound must be positive. {self.l2_norm_clip} is provided.'
+    assert not (
+        self.use_loss_weight_scaling and self.noise_multiplier != 0.0
+    ), 'Noise multiplier must be 0.0 when use_loss_weight_scaling is True.'
 
     mdl_vars_nograd, inputs = mdl_vars_nograd_and_inputs
     inputs = self._prepare_inputs(inputs)
@@ -454,10 +486,15 @@ class DpSgdStochasticGradient(BaseStochasticGradient):
       (values, aux), grads = grad_fn(
           mdl_vars_grad, (mdl_vars_nograd, new_inputs), inner_prng_keys[index]
       )
+      clipping_bound_scaling = _clipping_bound_scaling(
+          self.use_loss_weight_scaling, loss_weight=aux.loss_weight
+      )
 
       # Clip and aggregate gradients.
       grads, dp_aux_info, _ = self._clip_and_mean_gradients(
-          grads, aux.loss_weight[0] * self.l2_norm_clip, microbatch_splits
+          grads,
+          clipping_bound_scaling * self.l2_norm_clip,
+          microbatch_splits,
       )
       # Aggregate values and aux.
       values = jax.tree_map(jax.tree_util.Partial(jnp.mean, axis=0), values)
@@ -506,7 +543,12 @@ class DpSgdStochasticGradient(BaseStochasticGradient):
           / batch_size
           * (1.0 if self.normalize_gradients else self.l2_norm_clip)
       )
-      grads = self._add_noise(grads, noise_stddev, aux.loss_weight, prng_key)
+      grads = self._add_noise(
+          grads,
+          noise_stddev,
+          _clipping_bound_scaling(self.use_loss_weight_scaling),
+          prng_key,
+      )
     return (values, aux), grads
 
 