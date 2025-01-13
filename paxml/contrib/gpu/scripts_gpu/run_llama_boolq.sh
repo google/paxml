9383da272bb03173b99d75b6e0bdbea6a4d010df
Benjamin Chetioui <bchetioui@google.com>
2025-01-13 15:23:56 -0800

    Automated Code Change
    PiperOrigin-RevId: 715127449

--- paxml/contrib/gpu/scripts_gpu/run_llama_boolq.sh
+++ paxml/contrib/gpu/scripts_gpu/run_llama_boolq.sh
@@ -81,7 +81,6 @@ BASE_XLA_FLAGS=${BASE_XLA_FLAGS:-"\
 	--xla_gpu_enable_latency_hiding_scheduler=true \
 	--xla_gpu_enable_triton_gemm=false \
 	--xla_gpu_enable_highest_priority_async_stream=true \
-	--xla_gpu_enable_triton_softmax_fusion=false \
 	--xla_gpu_all_reduce_combine_threshold_bytes=51200 \
 	--xla_gpu_graph_level=0"}
 export XLA_FLAGS="$BASE_XLA_FLAGS ${XLA_FLAGS:-}"
